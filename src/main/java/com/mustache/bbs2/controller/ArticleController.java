787dea83b1a068b07932d58ced653c7b8f7dd844
soonmineom <skdlfma123@gmail.com>
2022-11-08 17:07:26 +0900

    회고 커밋

---
+++ src/main/java/com/mustache/bbs2/controller/ArticleController.java
@@ -0,0 +1,69 @@
+package com.mustache.bbs2.controller;
+
+import com.mustache.bbs2.domain.dto.ArticleDto;
+import com.mustache.bbs2.domain.entity.Article;
+import com.mustache.bbs2.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/articles")
+@Slf4j
+public class ArticleController {
+
+//    @Autowired
+//    ArticleRepository articleRepository;
+
+    private final ArticleRepository articleRepository;
+
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    @GetMapping(value = "/list")
+    public String list(Model model) {
+        List<Article> articles = articleRepository.findAll();
+        model.addAttribute("articles",articles);
+        return "articles/list";
+    }
+
+    @GetMapping("")
+    public String index() {
+        return "redirect:/articles/list";
+    }
+
+    @GetMapping(value = "/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+
+    @GetMapping("/{id}")
+    public String selectSingle(@PathVariable Long id, Model model) {
+        Optional<Article> optArticle = articleRepository.findById(id);
+        if (!optArticle.isEmpty()) {
+            model.addAttribute("article",optArticle.get());
+            return "articles/show";
+        } else {
+            return "articles/error";
+        }
+    }
+
+    @PostMapping(value = "/posts")
+    public String createArticle(ArticleDto articleDto) {
+        log.info(articleDto.toString());
+        Article article = articleDto.toEntity();
+        articleRepository.save(article);
+        return String.format("redirect:/articles/%d",article.getId());
+    }
+
+
+}
---
+++ src/main/java/com/mustache/bbs2/controller/MustacheController.java
@@ -0,0 +1,23 @@
+package com.mustache.bbs2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class MustacheController {
+
+    @GetMapping(value = "/hi")
+    public String mustacheCon(Model model) {
+        model.addAttribute("username","soonmin");
+        return "greetings";
+    }
+
+    @GetMapping(value = "/hi/{id}")
+    public String mustacheCon2(@PathVariable String id, Model model) {
+        model.addAttribute("username","soonmin");
+        model.addAttribute("id",id);
+        return "greetings";
+    }
+}
